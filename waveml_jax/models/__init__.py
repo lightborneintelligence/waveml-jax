efbbcdfc4ea9de15d68cea061f62b2fbaebfddf2
Jesus Carrasco <Jesuscarrasco@lightborneintelligence.org>
2026-01-15 19:29:41 -0600

    Update __init__.py

--- waveml_jax/models/__init__.py
+++ waveml_jax/models/__init__.py
@@ -1,84 +1,80 @@
-# “””
+"""
 WaveML Models
-
+=============
 Lightborne Intelligence
 
 Wave-native and baseline models:
-
 - waveseq: Wave-native sequence model with ERA
 - wave_rf: Wave receptive field for signal processing
 - baselines: RNN, LSTM, GRU for comparison
-  “””
+"""
 
 from .waveseq import (
-WaveSeqParams,
-init_waveseq_params,
-waveseq_step,
-waveseq_forward,
-WaveSeqCell,
-WaveSeq,
-detect_collapse,
+    WaveSeqParams,
+    init_waveseq_params,
+    waveseq_step,
+    waveseq_forward,
+    WaveSeqCell,
+    WaveSeq,
+    detect_collapse,
 )
 
 from .wave_rf import (
-wave_conv,
-wave_conv_with_era,
-WaveRFParams,
-init_waverf_params,
-waverf_layer,
-init_waverf_stack,
-waverf_forward,
-WaveRF,
+    wave_conv,
+    wave_conv_with_era,
+    WaveRFParams,
+    init_waverf_params,
+    waverf_layer,
+    init_waverf_stack,
+    waverf_forward,
+    WaveRF,
 )
 
 from .baselines import (
-RNNParams,
-init_rnn_params,
-rnn_step,
-rnn_forward,
-LSTMParams,
-LSTMState,
-init_lstm_params,
-lstm_step,
-lstm_forward,
-GRUParams,
-init_gru_params,
-gru_step,
-gru_forward,
-detect_baseline_collapse,
+    RNNParams,
+    init_rnn_params,
+    rnn_step,
+    rnn_forward,
+    LSTMParams,
+    LSTMState,
+    init_lstm_params,
+    lstm_step,
+    lstm_forward,
+    GRUParams,
+    init_gru_params,
+    gru_step,
+    gru_forward,
+    detect_baseline_collapse,
 )
 
-**all** = [
-# WaveSeq
-‘WaveSeqParams’,
-‘init_waveseq_params’,
-‘waveseq_step’,
-‘waveseq_forward’,
-‘WaveSeqCell’,
-‘WaveSeq’,
-‘detect_collapse’,
-# Wave RF
-‘wave_conv’,
-‘wave_conv_with_era’,
-‘WaveRFParams’,
-‘init_waverf_params’,
-‘waverf_layer’,
-‘init_waverf_stack’,
-‘waverf_forward’,
-‘WaveRF’,
-# Baselines
-‘RNNParams’,
-‘init_rnn_params’,
-‘rnn_step’,
-‘rnn_forward’,
-‘LSTMParams’,
-‘LSTMState’,
-‘init_lstm_params’,
-‘lstm_step’,
-‘lstm_forward’,
-‘GRUParams’,
-‘init_gru_params’,
-‘gru_step’,
-‘gru_forward’,
-‘detect_baseline_collapse’,
+__all__ = [
+    "WaveSeqParams",
+    "init_waveseq_params",
+    "waveseq_step",
+    "waveseq_forward",
+    "WaveSeqCell",
+    "WaveSeq",
+    "detect_collapse",
+    "wave_conv",
+    "wave_conv_with_era",
+    "WaveRFParams",
+    "init_waverf_params",
+    "waverf_layer",
+    "init_waverf_stack",
+    "waverf_forward",
+    "WaveRF",
+    "RNNParams",
+    "init_rnn_params",
+    "rnn_step",
+    "rnn_forward",
+    "LSTMParams",
+    "LSTMState",
+    "init_lstm_params",
+    "lstm_step",
+    "lstm_forward",
+    "GRUParams",
+    "init_gru_params",
+    "gru_step",
+    "gru_forward",
+    "detect_baseline_collapse",
 ]
